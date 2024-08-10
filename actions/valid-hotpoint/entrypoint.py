0e3eec314eddb94514fad14032f1fb2de5791cc2
Jackson <jackson.duarte@hotmart.com>
2024-08-10 19:24:22 -0300

    Check Hotpoint

---
+++ actions/valid-hotpoint/entrypoint.py
@@ -0,0 +1,28 @@
+import sys
+import yaml
+
+def check_hotpoint_in_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+
+        if 'env' in data and 'HOTPOINT' in data['env'] and data['env']['HOTPOINT'] is True:
+            print("The property 'env.HOTPOINT = true' is present in the file.")
+        else:
+            print("Error: The property 'env.HOTPOINT = true' is NOT present in the file.")
+            sys.exit(1)
+
+    except FileNotFoundError:
+        print(f"Error: File not found at path {file_path}")
+
+    except yaml.YAMLError as exc:
+        print(f"Error parsing YAML file: {exc}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python entrypoint.py <path_to_yaml_file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    check_hotpoint_in_yaml(file_path)
