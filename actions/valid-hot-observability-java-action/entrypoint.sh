c8b1c87ef46b587b1126834666340018f928dc4c
Jackson <jackson.duarte@hotmart.com>
2024-08-10 19:02:26 -0300

    Check xss protection

--- actions/valid-hot-observability-java-action/entrypoint.sh
+++ actions/valid-hot-observability-java-action/entrypoint.sh
@@ -10,7 +10,9 @@ fi
 if grep -q "<artifactId>hot-observability-spring-boot-starter</artifactId>" "$POM_FILE"; then
   echo "Dependency 'hot-observability-spring-boot-starter' found in $POM_FILE"
 else
+  echo "###"
   echo "Error: Dependency 'hot-observability-spring-boot-starter' NOT found in $POM_FILE"
   echo "see https://techdeck.hotmart.co/catalog/default/component/heimdall/docs/findings/app_without_observability_java/"
+  echo "###"
   exit 1
 fi
